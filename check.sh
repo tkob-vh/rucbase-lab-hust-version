f50c16a34adae83a4237a4f1b9b0893a40cfb1e2
杨茂林 <8575543@qq.com>
2023-10-02 02:17:37 +0800

    Edit by browser

--- check.sh
+++ check.sh
@@ -1,6 +1,20 @@
-cd rmdb
+#!/bin/bash
+
+if [ -f build ];then
+rm -rf build
+fi
+
 mkdir build
 cd build
 cmake ..
-make -j4
-if 
+make 
+if [ $? == 0 ]; then 
+if [ -f bin/disk_manager_test ]; then
+chmod +x bin/disk_manager_teset
+./bin/disk_manager_test
+else 
+echo "没有编译生成评测程序，请检查代码！"
+fi
+echo "make没有成功，请根据make提示，修改后程序"
+fi
+
