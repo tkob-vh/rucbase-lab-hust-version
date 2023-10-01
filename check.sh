2c7550efbb5974ba569afe2b0843157021e566bb
杨茂林 <8575543@qq.com>
2023-10-02 02:28:36 +0800

    Edit by browser

--- check.sh
+++ check.sh
@@ -6,8 +6,11 @@ fi
 
 mkdir build
 cd build
-cmake .. 
-make 
+touch cmake_err.txt
+touch make_err.txt
+cmake .. > cmake_err.txt 2>&1 >/dev/null
+if [ $? == 0 ]; then 
+make > make_err.txt 2>&1 >/dev/null
 if [ $? == 0 ]; then 
 if [ -f bin/disk_manager_test ]; then
 chmod +x bin/disk_manager_teset
@@ -15,6 +18,11 @@ chmod +x bin/disk_manager_teset
 else 
 echo "没有编译生成评测程序，请检查代码！"
 fi
-echo "make没有成功，请根据make提示，修改后程序"
+else 
+echo "make没有成功，请根据make提示，修改后程序:"
+cat make_err.txt
 fi
-
+else 
+echo "cmake出错，以下是提示信息："
+cat cmake_err.txt
+fi
