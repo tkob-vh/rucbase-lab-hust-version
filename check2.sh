b52e0ea487aaaa35336510b057f7c1b7fb9c3b1b
杨茂林 <8575543@qq.com>
2023-10-02 11:55:34 +0800

    Edit by browser

--- check2.sh
+++ check2.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 cd rmdb
 
-if [ -d build ];then
+
+if [ -f ./build/Makefile ];then
+cd build
+touch cmake_err.txt
+touch make_err.txt
+make > make_err.txt 2>&1 >/dev/null
+if [ $? == 0 ]; then 
+if [ -f ./bin/lru_replacer_test ]; then
+chmod +x ./bin/lur_replacer_test
+./bin/lru_replacer_test --gtest_print_time=0
+fi 
+else 
+echo "make没有成功，请根据make提示，修改后程序:"
+cat make_err.txt
+fi 
+else
+if [ -d ./build ]; then 
 rm -rf build
-fi
+fi 
 
 mkdir build
 cd build
@@ -13,8 +29,8 @@ cmake .. > cmake_err.txt 2>&1 >/dev/null
 if [ $? == 0 ]; then 
 make > make_err.txt 2>&1 >/dev/null
 if [ $? == 0 ]; then 
-if [ -f ./bin/disk_manager_test ]; then
-chmod +x ./bin/disk_manager_test
+if [ -f ./bin/lru_replacer_test ]; then
+chmod +x ./bin/lru_replacer_test
 ./bin/lru_replacer_test --gtest_print_time=0
 else 
 echo "没有编译生成评测程序，请检查代码！"
@@ -26,4 +42,5 @@ fi
 else 
 echo "cmake出错，以下是提示信息："
 cat cmake_err.txt
+fi
 fi
