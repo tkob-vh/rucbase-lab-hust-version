b4a3407e4656a278d88148e64e6e3e03acb8bf9c
杨茂林 <8575543@qq.com>
2023-10-02 11:58:52 +0800

    Edit by browser

--- check4.sh
+++ check4.sh
@@ -1,3 +1,46 @@
+#!/bin/bash
+cd rmdb
 
 
-./bin/rm_gtest --gtest_print_time=0
+if [ -f ./build/Makefile ];then
+cd build
+touch cmake_err.txt
+touch make_err.txt
+make > make_err.txt 2>&1 >/dev/null
+if [ $? == 0 ]; then 
+if [ -f ./bin/rm_gtest  ]; then
+chmod +x ./bin/rm_gtest 
+./bin/rm_gtest  --gtest_print_time=0
+fi 
+else 
+echo "make没有成功，请根据make提示，修改后程序:"
+cat make_err.txt
+fi 
+else
+if [ -d ./build ]; then 
+rm -rf build
+fi 
+
+mkdir build
+cd build
+touch cmake_err.txt
+touch make_err.txt
+cmake .. > cmake_err.txt 2>&1 >/dev/null
+if [ $? == 0 ]; then 
+make > make_err.txt 2>&1 >/dev/null
+if [ $? == 0 ]; then 
+if [ -f ./bin/rm_gtest  ]; then
+chmod +x ./bin/rm_gtest 
+./bin/rm_gtest  --gtest_print_time=0
+else 
+echo "没有编译生成评测程序，请检查代码！"
+fi
+else 
+echo "make没有成功，请根据make提示，修改后程序:"
+cat make_err.txt
+fi
+else 
+echo "cmake出错，以下是提示信息："
+cat cmake_err.txt
+fi
+fi
